eef8911fc631fbd6612d8c06b832622856549fac
suraj jaiswal <jaiswaljesus384@gmail.com>
2024-08-04 17:15:50 +0530

    cross  problem solved

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 from route.user import user_bp  # Import the user blueprint
 
 app = Flask(__name__)
-
+CORS(app)
 # Register the user blueprint with the app
 app.register_blueprint(user_bp)
 
